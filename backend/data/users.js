7d7b78e01dd47920e919330f453ffab40cf94f56
Jose de la Cruz <jose.delacruz55555@gmail.com>
2022-05-03 20:05:40 -0400

    firebase id is now mongoDB _id

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,45 +1,51 @@
-const collections = require('../config/mongoCollections');
+const collections = require("../config/mongoCollections");
 const users = collections.users;
+const ObjectId = require("mongodb").ObjectId;
 
 module.exports = {
-    async create(firebaseuid, userEmail) {
-        if(!userEmail || typeof userEmail !== 'string') throw 'Must provice a valid userEmail';
-        if(!firebaseuid || typeof firebaseuid !== 'string') throw 'Must provice a valid firebaseuid';
-        if(await this.userExists(firebaseuid)) throw 'user already in use'
+  async create(firebaseuid, userEmail) {
+    if (!userEmail || typeof userEmail !== "string") throw "Must provice a valid userEmail";
+    if (!firebaseuid || typeof firebaseuid !== "string") throw "Must provice a valid firebaseuid";
+    if (await this.userExists(firebaseuid)) throw "user already in use";
 
-        const userCollection = await users();
-        let newUser = {
-            userEmail: userEmail,
-            firebaseuid: firebaseuid
-        }
+    try {
+      let uid = ObjectId(firebaseuid);
+    } catch (error) {
+      console.log(error);
+    }
+    const userCollection = await users();
+    let newUser = {
+      userEmail: userEmail,
+      _id: firebaseuid
+    };
 
-        const insertInfo = await userCollection.insertOne(newUser);
-        if(insertInfo.insertedCount === 0) throw `Could not add user ${userEmail}`;
+    const insertInfo = await userCollection.insertOne(newUser);
+    if (insertInfo.insertedCount === 0) throw `Could not add user ${userEmail}`;
 
-        return insertInfo.insertedId;
-    },
+    return insertInfo.insertedId;
+  },
 
-    async getAllUsers(){
-        const userCollection = await users();
-        const arr = await userCollection.find({}).toArray();
-        return arr;
-    },
+  async getAllUsers() {
+    const userCollection = await users();
+    const arr = await userCollection.find({}).toArray();
+    return arr;
+  },
 
-    async userExists(userID){
-        if(!userID || typeof userID !== 'string') throw 'Must provide a valid userID';
+  async userExists(userID) {
+    if (!userID || typeof userID !== "string") throw "Must provide a valid userID";
 
-        const userCollection = await users();
-        const user = await userCollection.findOne({userID: userID});
-        if(user === null) return false;
-        return true;
-    },
+    const userCollection = await users();
+    const user = await userCollection.findOne({ userID: userID });
+    if (user === null) return false;
+    return true;
+  },
 
-    async getUser(userID){
-        if(!userID || typeof userID !== 'string') throw 'Must provice a valid userID';
+  async getUser(userID) {
+    if (!userID || typeof userID !== "string") throw "Must provice a valid userID";
 
-        const userCollection = await users();
-        const user = await userCollection.findOne({userID: userID});
-        if(user === null) return null;
-        return user;
-    },
-}
+    const userCollection = await users();
+    const user = await userCollection.findOne({ userID: userID });
+    if (user === null) return null;
+    return user;
+  },
+};
