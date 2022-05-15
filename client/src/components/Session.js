4a5b27bf801339db1a3f2c68879f320d23348b10
Jose de la Cruz <jose.delacruz55555@gmail.com>
2022-05-15 17:58:20 -0400

    init

--- client/src/components/Session.js
+++ client/src/components/Session.js
@@ -6,89 +6,98 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function Session(props) {
-    const { currentUser } = useContext(AuthContext);
-    const { id } = useParams();
-    const socketRef = useRef();
-    const [session, setSession] = useState(undefined);
-    const [chars, setChars] = useState([]);
-    const [sessionOpen, setSessionOpen] = useState(false);
-    console.log(props);
+  const { currentUser } = useContext(AuthContext);
+  const { id } = useParams();
+  const socketRef = useRef();
+  const [session, setSession] = useState(undefined);
+  const [chars, setChars] = useState([]);
+  const [sessionOpen, setSessionOpen] = useState(false);
+  console.log(props);
 
-    //connect socket.io
-    useEffect(() => {
-        socketRef.current = io("/");
-        return () => {
-            socketRef.current.disconnect();
-        };
-    }, []);
-
-    //get session data
-    useEffect(() => {
-        const getSession = async () => {
-            try {
-                let response = await axios.get(`http://localhost:5000/session/${id}/${currentUser.uid}`);
-                setSession(response.data);
-                console.log(response.data);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        if (currentUser) {
-            getSession();
-        }
-    }, [currentUser, id]);
-
-    const DM_join = () => {
-        setSessionOpen(true);
-        socketRef.current.emit("DM_join", session.sessionName);
+  //connect socket.io
+  useEffect(() => {
+    socketRef.current = io("/");
+    return () => {
+      socketRef.current.disconnect();
     };
+  }, []);
 
-    const healthChange = (health, charId) => {
-        socketRef.current.emit("healthChange", health, session.sessionName, charId);
+  //get session data
+  useEffect(() => {
+    const getSession = async () => {
+      try {
+        let response = await axios.get(`http://localhost:5000/session/${id}/${currentUser.uid}`);
+        setSession(response.data);
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
     };
-
-    useEffect(() => {
-        socketRef.current.on("user_join", ({charId, charName, health}) => {
-            console.log("help")
-            setChars((prev) => [...prev, {charId, charName, health}]);
-        });
-
-    }, []);
-
-    console.log(sessionOpen)
-    if(!session){
-        return <div>Loading...</div>
+    if (currentUser) {
+      getSession();
     }
-    if(!sessionOpen){
-        return(
-            <div>
-                <h1>{session.sessionName}</h1>
-                <Button onClick={DM_join}>Open Session</Button>
-            </div>
-        )
-    }
-    return(
-        <div>
-            <h1>{session.sessionName}</h1>
-            <div>
-                {chars.map((char) => (
-                    <div key={`${char.charId}`}>
-                        <h3>{char.charName}</h3>
-                        <form onSubmit={(e) => {
-                            e.preventDefault();
-                            console.log(e.target.health.value);
-                            healthChange(e.target.health.value, char.charId)
-                        }}>
-                            <input type="number" name="health" defaultValue={char.health} />
-                            <Button type="submit">Change Health</Button>
-                        </form>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-    
+  }, [currentUser, id]);
 
+  const DM_join = () => {
+    setSessionOpen(true);
+    socketRef.current.emit("DM_join", session.sessionName);
+  };
+
+  const healthChange = (health, charId) => {
+    socketRef.current.emit("healthChange", health, session.sessionName, charId);
+  };
+
+  useEffect(() => {
+    socketRef.current.on("user_join", ({ charId, charName, health, max_health }) => {
+      console.log("User Join");
+      setChars((prev) => [...prev, { charId, charName, health, max_health }]);
+      console.log(JSON.stringify(chars[0], null, 4));
+    });
+
+    socketRef.current.on("user_left", ({ charId }) => {
+      console.log(`front end recived user_left \n Name: ${charId}`);
+      //   let newChars = [...chars];
+      let i = chars.findIndex((elem) => elem.charId === charId);
+      console.log(`Users: ${chars[0]}`);
+      console.log(`User that left: ${JSON.stringify(chars[i], null, 4)}`);
+    });
+  }, []);
+
+  console.log(sessionOpen);
+  if (!session) {
+    return <div>Loading...</div>;
+  }
+  if (!sessionOpen) {
+    return (
+      <div>
+        <h1>{session.sessionName}</h1>
+        <Button onClick={DM_join}>Open Session</Button>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>{session.sessionName}</h1>
+      <div>
+        {chars.map((char) => (
+          <div key={`${char.charId}`}>
+            <h3>{char.charName}</h3>
+            <h3>{`Max Health: ${char.max_health}`}</h3>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                console.log(e.target.health.value);
+                healthChange(e.target.health.value, char.charId);
+              }}
+            >
+              <input type="number" name="health" defaultValue={char.health} />
+              <Button type="submit">Change Health</Button>
+            </form>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Session;
